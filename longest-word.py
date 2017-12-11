52b4815a8aa479cff60cec3d2b18e2a522e0577e
maxwikander <wikanderm@gmail.com>
2017-12-11 19:15:26 +0100

    final

--- longest-word.py
+++ longest-word.py
@@ -1,2 +1,18 @@
-foo = "bar"
-print(foo)
+import re
+def LongestWord(sen): 
+    regex = re.compile('[^a-zA-Z]')
+    #First parameter is the replacement, second parameter is your input string
+    regex.sub('', 'ab3d*E')
+    #Out: 'abdE'    
+    sen_list = sen.split(" ")
+    max_len = 0
+    for sen in sen_list:
+        sen = regex.sub('', sen)
+        if len(sen) > max_len:
+            max_len_word = sen
+            max_len = len(sen)
+
+    return max_len_word
+    
+# keep this function call here  
+print LongestWord(raw_input())
