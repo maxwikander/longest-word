06aa3092377802152c95feb519d0bd8e92956276
maxwikander <wikanderm@gmail.com>
2017-12-11 19:23:33 +0100

    Code cleaning

--- longest-word.py
+++ longest-word.py
@@ -1,18 +1,21 @@
 import re
 def LongestWord(sen): 
+    # defining valid characters
     regex = re.compile('[^a-zA-Z]')
-    #First parameter is the replacement, second parameter is your input string
-    regex.sub('', 'ab3d*E')
-    #Out: 'abdE'    
+
+    # parsing input to list
     sen_list = sen.split(" ")
+
+    # initiating comparison loop
     max_len = 0
     for sen in sen_list:
+        # using re.sub to remove unvalid chracters from string
         sen = regex.sub('', sen)
         if len(sen) > max_len:
             max_len_word = sen
-            max_len = len(sen)
+            max_len = len(max_len_word)
 
     return max_len_word
     
-# keep this function call here  
-print LongestWord(raw_input())
+# function call
+print LongestWord(input())
